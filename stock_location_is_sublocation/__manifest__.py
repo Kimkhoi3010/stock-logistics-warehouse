cbe04481a9074fba0a33770e6bb3cb142ce9b133
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-03-04 14:43:25 +0530

    [ADD] stock_location_is_sublocation

--- stock_location_is_sublocation/__manifest__.py
+++ stock_location_is_sublocation/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright 2020-2021 Camptocamp SA
+# Copyright 2016-2025 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Stock Helpers",
-    "summary": "Add methods shared between various stock modules",
-    "version": "17.0.1.1.1",
+    "name": "Stock Location Is Sublocation",
+    "summary": "Add method to check stock location is sublocation",
+    "version": "18.0.1.0.0",
     "development_status": "Production/Stable",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Camptocamp, ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Hidden",
     "depends": ["stock"],
